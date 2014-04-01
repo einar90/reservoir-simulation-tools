1ccfe4451823d894f0d6a726a5552757b2326a60
Einar Baumann <einar.baumann@gmail.com>
2014-04-01 19:12:09 +0200

    Added some prettyprinting

--- python/eclipse_summary_plotter.py
+++ python/eclipse_summary_plotter.py
@@ -17,15 +17,20 @@ def list_summary_files():
         summary_files.pop()  # Deleting empty element at end
     # Removing ./ and .UNSMRY
     summary_files = [sumfile[2:-7] for sumfile in summary_files]
-    print 'The following files are available in the active folder:'
+
+    print '\n',
+    print '|-----------------------------------------------------------------|'
+    print '| The following files are available in the active folder:         |'
+    print '|-----------------------------------------------------------------|'
     for i in range(len(summary_files)):
-        print '(%i) %s' % (i, summary_files[i])
+        print '| (%i) %s' % (i, summary_files[i])
+    print '|-----------------------------------------------------------------|'
     return summary_files
 
 
 def get_file_name():
     summary_files = list_summary_files()
-    input_file = raw_input('Filename or number: ')
+    input_file = raw_input('\nFilename or number: ')
     try:
         file_number = int(input_file)
         filename = summary_files[file_number]
@@ -35,7 +40,6 @@ def get_file_name():
 
 
 def get_wells(filename):
-    print "The following wells are available:"
     wellist = subprocess.check_output([
                                       "ecl_summary",
                                       filename,
@@ -47,17 +51,25 @@ def get_wells(filename):
     wellist.pop()  # Last element is \n
     wellist = [well.strip().split(':') for well in wellist]
     wellist = [well[1] for well in wellist]
+
+    print '\n\n',
+    print '|-----------------------------------------------------------------|'
+    print '| The following wells are available:                              |'
+    print '|-----------------------------------------------------------------|'
+    print '| ',
     for well in range(len(wellist)):
         print wellist[well] + '\t',
-        if (well % 8) == 0 and well != 0:
-            print '\n',
-    print '\n',
-    wells = raw_input('Select wells (space-separated): ').split(' ')
+        if (well % 6) == 0 and well != 0:
+            print '\n| ',
+    print '\n|-----------------------------------------------------------------|'
+    wells = raw_input('\nSelect wells (space-separated): ').split(' ')
+    print '\n'
     return wells
 
 
 def get_properties(filename, wells):
     print "The following properties are available for the selected wells:"
+    print '==================================================================='
     proplist = []
     for well in wells:
         proplist.append(subprocess.check_output([
@@ -73,7 +85,9 @@ def get_properties(filename, wells):
         proplist[well] = [prop[0] for prop in proplist[well]]
 
     for well in range(len(proplist)):
-        print 'Properties for well ' + wells[well]
+        print '|-----------------------------------------------------------------|'
+        print '| Properties for well ' + wells[well]
+        print '|-----------------------------------------------------------------|'
         #print proplist[well]
         for prop in range(len(proplist[well])):
             print proplist[well][prop] + '\t',
@@ -151,7 +165,7 @@ def create_value_vectors(valuelines, props, wells):
 
 
 def plot_values(propstrings, values, time, props, wells):
-    plt.Figure()
+    fig = plt.figure(figsize=(10, 5))
     plots = []
     for i in range(len(props)):
         for j in range(len(wells)):
@@ -159,13 +173,20 @@ def plot_values(propstrings, values, time, props, wells):
             plots.append(line)
 
     plt.legend(plots, propstrings, loc=2)
+    fig1 = plt.gcf()
+    fig1.set_size_inches(10.5, 10.5)
     plt.show()
+
+    save = raw_input('Save figure (y/n)? ')
+    if save == 'y' or save == 'Y':
+        figname = raw_input('Figure name: ')
+        fig1.savefig('%s.pdf' % figname)
 
     next_action = raw_input('Quit program (q) or write data to csv (w)? ')
 
-    if next_action == 'q':
+    if next_action == 'q' or next_action == 'Q':
         plt.close()
-    elif next_action == 'w':
+    elif next_action == 'w' or next_action == 'W':
         plt.close()
         write_data_to_csv(propstrings, values, time, wells, props)
 
@@ -200,13 +221,15 @@ def get_action():
 filename = get_file_name()  # Getting file from user input
 
 # Asking for well/field data
-field_data = (raw_input('Plot field (f) or well (w) data? ') == 'f')
+field_data = (raw_input('\n Plot field (f) or well (w) data? ') == 'f')
 
 # Getting well and property lists
 if field_data:
+    print 'Selected field data.'
     props = propstrings = get_field_props(filename)
     wells = ['FIELD']
 else:
+    print 'Selected well data.'
     wells = get_wells(filename)  # Getting wells from user input
     props = get_properties(filename, wells)  # Getting props from user input
     propstrings = create_propery_selection_string(props, wells)  # Parsing props
