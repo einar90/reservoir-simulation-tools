35580621c1227135cf82eccda073e2500c92f681
Jugurtha Hadjar <jugurtha.hadjar@gmail.com>
2015-06-30 23:38:52 +0100

    Update eclipse_summary_converter.py
    * Added directory choice, defaults to current working directory.
    * Untangled list_summary_files and get_file_name. Latter was calling the former.
    * Narrowed exceptions in get_file_name.
    * Program now takes an argument for the path to search files in.
    * Made list_summary_files take the `path` argument to search for summary files elsewhere.
    
    TODO:
    * make list_summary_files platform independant (get rid of `find`) using os module.

--- python/eclipse_summary_converter.py
+++ python/eclipse_summary_converter.py
@@ -1,20 +1,24 @@
 '''
-Uses the 'ecl_summary' command found in the Ensambles ERT software package
-created by statoil to convert data from an eclipse summary file to a csv format.
-Requires both a *.UNSMRY and *.SMSPEC file to be present.
+Uses the `ecl_summary` command found in the `Ensemble based Reservoir Tool` - ERT 
+software package created by Statoil to convert data from an Eclipse Summary file 
+to a .CSV format. Requires both a *.UNSMRY and *.SMSPEC file to be present.
 
-https://github.com/Ensembles/ert
+ERT software repository:
+    https://github.com/Ensembles/ert
 '''
 
 from matplotlib import pyplot as plt
 #from __future__ import print_function
 import subprocess
+import sys
 import os
 
-def list_summary_files():
-    summary_files = subprocess.check_output(['find', '.', '-name', '*.UNSMRY'])
+def list_summary_files(path):
+    '''
+        List .UNSMRY files in `path` directory tree.
+    '''
+    summary_files = subprocess.check_output(['find', path, '-name', '*.UNSMRY'])
     summary_files = summary_files.rstrip('\n').split('\n')
-    #summary_files = filter(None, summary_files)
     # Removing ./ and .UNSMRY
     summary_files = [sumfile[2:-7] for sumfile in summary_files]
 
@@ -27,17 +31,22 @@ def list_summary_files():
     print '|-----------------------------------------------------------------|'
     return summary_files
 
-def get_file_name():
-    summary_files = list_summary_files()
+def get_file_name(summary_files):
+    '''
+        Get the user's chosen file from the files list.
+    '''
     input_file = raw_input('\nFilename or number: ')
     try:
         file_number = int(input_file)
         filename = summary_files[file_number]
-    except Exception:
-        filename = input_file
+    except (ValueError, IndexError):
+        filename = input_file if input_file in summary_files else None
     return filename
 
 def get_properties(filename):
+    '''
+        Run ecl_summary on the file to get properties.
+    '''
     proplist = subprocess.check_output(["ecl_summary", filename, "--list"])
     proplist = proplist.replace('\n', ' ')
     proplist = proplist.split(' ')
@@ -83,6 +92,12 @@ def save_to_csv(headers, valuelist, filename):
     f.close()
     print 'Successfully created csv file ' + output_filename
 
+try:
+    path = sys.argv[1]
+except IndexError:
+    path = os.getcwd()
+
+summary_files = list_summary_files()
 
 filename = get_file_name()  # Getting file from user input
 proplist = get_properties(filename)  # Generate list of properties in file
